7e70d71c83717de7c6b71f7e5eca8a885b27024d
Sumedh Inamdar <sinamdar@gmail.com>
2022-02-26 22:04:32 -0800

    Added Header, CV container, Footer components and styling. Setup ESlint / Prettier / Unicorn.

---
+++ src/components/CVContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/CVContainer.css';
+
+export default class CVContainer extends Component {
+  render() {
+    return (
+      <div id="cvCont">
+        <div></div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/Header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>CV Maker</h1>
+      </header>
+    );
+  }
+}
